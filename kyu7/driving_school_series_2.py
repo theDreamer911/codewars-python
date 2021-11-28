f856e8456d6129249184c2ad6209648bc3a03966
Handhika Yanuar Pratama <thetrulydreamer@gmail.com>
2021-11-28 16:40:37 +0700

    add best and one liner solution

--- kyu7/driving_school_series_2.py
+++ kyu7/driving_school_series_2.py
@@ -11,3 +11,17 @@ def cost(mins):
     return price
 
 print(cost(273))
+
+########### BEST SOLUTION ######################
+import math
+
+def best(mins):
+    return 30 + 10 * math.ceil(max(0, mins - 60 - 5) / 30)
+
+
+########### ONE LINER ######################
+def one_liner(mins):
+    return 30 if mins <= 60 else 30 + 10*int((mins - 60 + 24) / 30)
+
+def one_liner_second(mins):
+    return max(0, -(-(mins - 65) // 30)) * 10 + 30
