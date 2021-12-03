87f3848a93a4c39bd183f3a39ef5ec70246d0d53
Handhika Yanuar Pratama <thetrulydreamer@gmail.com>
2021-12-04 06:16:10 +0700

    review

--- kyu7/drone_fly_by.py
+++ kyu7/drone_fly_by.py
@@ -1,2 +1,8 @@
+# 3/12
 def fly_by(lamps, drone):
     return lamps.replace('x', 'o', drone.index('T') + 1)
+
+# 4/12
+fly_by = lambda lamps, drone: lamps.replace("x", "o", len(drone))
+
+print(fly_by('xxxxxx', '====T'))
