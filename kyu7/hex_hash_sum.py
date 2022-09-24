b2587539b358e94f5099b807140c443a7b72c1b8
theDreamer911 <thetrulydreamer@gmail.com>
2022-09-24 20:38:41 +0700

    best solution of hex hash sum

--- kyu7/hex_hash_sum.py
+++ kyu7/hex_hash_sum.py
@@ -5,3 +5,9 @@ def hex_hash(code):
     val_hex = val_encoded.hex()
     score = sum(map(int, re.findall("\d", val_hex)))
     return score
+
+
+def hex_hash(code):
+    return sum(int(d) for c in code for d in hex(ord(c)) if d.isdigit())
+
+print(hex_hash("Hello, World!"))
